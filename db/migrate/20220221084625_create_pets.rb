16f4a78cdcd27068c0c50208945d55bd2cdad55f
andrei mori <andrei.code.nz@gmail.com>
2022-02-21 18:33:03 +0900

    second try

--- db/migrate/20220221084625_create_pets.rb
+++ db/migrate/20220221084625_create_pets.rb
@@ -1,6 +1,11 @@
 class CreatePets < ActiveRecord::Migration[6.1]
   def change
     create_table :pets do |t|
+      t.string :name
+      t.string :description
+      t.string :species
+      t.string :breed
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
