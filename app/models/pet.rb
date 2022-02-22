7060b87082dc1a05c3060c96b61c2b145e00f804
andrei mori <andrei.code.nz@gmail.com>
2022-02-22 16:14:34 +0900

    seeds 95% complete, need testing

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,7 @@ class Pet < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2 }
   validates :description, presence: true
   validates :species, presence: true
-  validates :breed, presence: true
+  validates :character, presence: true
   validates :price, presence: true, numericality: { only_integer: true }, allow_blank: false
 
 end
