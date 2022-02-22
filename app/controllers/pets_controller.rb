bf7fec011f606b3bfff47a47b217c431965a159e
Taylor Mravic <taylor.mravic@gmail.com>
2022-02-22 14:43:22 +0900

    add pundit

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,17 @@
+class PetsController < ApplicationController
+  def index
+    @pets = policy_scope(Pet) # looks inside pet_policy resolve to find Pet.all, policy_scope is only used in index
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+    authorize @pet # authorize is used everywhere but index
+    @booking = Booking.new # because we are making a form on the show page, we need a blank booking
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :bookings # as renter
+  has_many :pets
+  has_many :bookings_as_owner, through: :pets, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
