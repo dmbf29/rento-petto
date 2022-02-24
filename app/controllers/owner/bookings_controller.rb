aed99b99ab0dccc7f0d05477bc09f54f5ccd584f
Taylor Mravic <taylor.mravic@gmail.com>
2022-02-24 13:06:53 +0900

    Owner validates bookings

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,5 +1,8 @@
 class Owner::BookingsController < ApplicationController
   def index
     @bookings = policy_scope([:owner, Booking])
+    # select one booking
+    # @booking.status = # whatver button user selects
   end
+
 end
