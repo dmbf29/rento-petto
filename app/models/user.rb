60ef913d4da67b1af90b675dc34d04f8ee397aa7
andrei mori <andrei.code.nz@gmail.com>
2022-02-24 22:13:10 +0900

    pet_photos_seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :bookings # as renter
   has_many :pets
   has_many :bookings_as_owner, through: :pets, source: :bookings
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
